0408cf40df66b7cf65941a9012c6cbc39d1b0cf6
Hoang Do <hoangdo.dh94@gmail.com>
2016-09-21 23:45:06 +0800

    init

---
+++ imports/ui/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+export const Index = () => (
+  <Jumbotron className="text-center">
+    <h2>Project Erox</h2>
+    <p>Something fun :v</p>
+  </Jumbotron>
+);
