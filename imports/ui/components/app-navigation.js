c9dfa0e4bafb836f0e13c45f5372b774135e6d70
Hoang Do <hoangdo.dh94@gmail.com>
2016-09-23 17:40:07 +0800

    use boostwatch paper theme

--- imports/ui/components/app-navigation.js
+++ imports/ui/components/app-navigation.js
@@ -10,7 +10,7 @@ export class AppNavigation extends React.Component {
   }
 
   render() {
-    return <Navbar>
+    return <Navbar bsStyle={ 'inverse' }>
       <Navbar.Header>
         <Navbar.Brand>
           <Link to="/">Erox</Link>
